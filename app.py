150550e636834763f03fc2e6e84e833b99832ac3
Kamali S <kamali9514@gmail.com>
2026-03-30 22:21:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -160,5 +160,6 @@ def service():
 # ==============================
 
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True) 
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
